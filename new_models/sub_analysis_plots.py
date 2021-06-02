de51e5df5fbdf5f4105443a01d56ce1b2f5c1cae
w-nicole <wnicole1549@gmail.com>
2021-06-02 15:07:45 -0700

    added attention plots

--- new_models/sub_analysis_plots.py
+++ new_models/sub_analysis_plots.py
@@ -7,12 +7,12 @@ def filter_nan_df(df):
     df = df[~(df['orig_prob'].isnull() | df['edited_prob'].isnull())]
     return df
 
-def to_negative_surprisal(prob_series):
-    return np.log10(prob_series)
+def to_surprisal(prob_series):
+    return -np.log10(prob_series)
 
 # 6/1 : https://seaborn.pydata.org/generated/seaborn.violinplot.html
 
-def prep_single_model_df_violin(model_name, df, is_neg_surprisal = False):
+def prep_single_model_df_violin(model_name, df, is_surprisal = False):
     
     """
     Convert each of the models to a yes/no dataset
@@ -23,8 +23,8 @@ def prep_single_model_df_violin(model_name, df, is_neg_surprisal = False):
         return [label for _ in range(len(df))]
 
     
-    orig_probs = to_negative_surprisal(df['orig_prob']) if is_neg_surprisal else df['orig_prob']
-    edited_probs = to_negative_surprisal(df['edited_prob']) if is_neg_surprisal else df['edited_prob']
+    orig_probs = to_surprisal(df['orig_prob']) if is_surprisal else df['orig_prob']
+    edited_probs = to_surprisal(df['edited_prob']) if is_surprisal else df['edited_prob']
     
     all_probs = pd.concat([orig_probs, edited_probs])
     yes_no_labels = yes_no_gen("Yes", df) + yes_no_gen("No", df)
@@ -36,7 +36,7 @@ def prep_single_model_df_violin(model_name, df, is_neg_surprisal = False):
     
     return this_model_probs_df
 
-def gen_violin_plots(all_changes_probs, use_neg_surprisal):
+def gen_violin_plots(all_changes_probs, use_surprisal):
     """
     all_changes_probs = The output of prep_probs.load_word_changes
         This is the DataFrame with the original/edited probabilities 
@@ -46,6 +46,6 @@ def gen_violin_plots(all_changes_probs, use_neg_surprisal):
     
     return pd.concat([
         prep_single_model_df_violin(model_name, filter_nan_df(raw_df), 
-                                   is_neg_surprisal = use_neg_surprisal)
+                                   is_surprisal = use_surprisal)
         for model_name, raw_df in all_changes_probs.items()
     ])
